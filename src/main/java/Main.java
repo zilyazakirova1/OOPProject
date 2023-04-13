0e475ce5db65977eb6b9d1429b6001439de8191f
Neto test <zilya.zakirova1991@gmail.com>
2023-04-13 18:19:39 +0300

    Changes

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import ru.netology.Post;
+public class Main {
+    public static void main (String[]args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.surname="Иванов";
+        post.patronymic="Иванович";
+        post.phonenumber= 790698000L;
+        post.passportnumber= 9612896332L;
+        post.subscription= "true";
+    }
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public long phonenumber;
+    public long passportnumber;
+    public String subscription;
+
+}
